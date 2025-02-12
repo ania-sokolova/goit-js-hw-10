2f26e3a06991b5e93f2feff285d583f98308d5cb
Ania Sokolova <ania2014sokolova@gmail.com>
2025-02-12 14:26:41 +0200

    hw10d

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,6 +1,6 @@
-// Описаний у документації
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector(".form");
 
